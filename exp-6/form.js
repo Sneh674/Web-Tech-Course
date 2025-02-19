0adc1b772da76c4a5f863806c29d1209c766dc38
Sneh674 <naiksneh6@gmail.com>
2025-02-19 10:49:29 +0530

    exp 6 started

---
+++ exp-6/form.js
@@ -0,0 +1,48 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    const data = {
+        name: formData.get("uname"),
+        email: formData.get("email"),
+        mobile: formData.get("mobile"),
+        file: formData.get("file") ? formData.get("file").name : "No file uploaded"
+    };
+
+    console.log("Form Submitted:", data);
+
+    alert("Form submitted successfully!");
+});
+
+const stars = document.querySelectorAll('.star');
+const ratingText = document.getElementById('ratingText');
+
+stars.forEach(star => {
+    star.addEventListener('click', () => {
+        const value = star.dataset.value;
+        ratingText.textContent = `Rating: ${value}`;
+        
+        stars.forEach(s => s.classList.remove('active'));
+        star.classList.add('active');
+        let prev = star;
+        while (prev.previousElementSibling) {
+            prev = prev.previousElementSibling;
+            prev.classList.add('active');
+        }
+    });
+
+    star.addEventListener('mouseover', () => {
+        stars.forEach(s => s.classList.remove('hover'));
+        star.classList.add('hover');
+        let prev = star;
+        while (prev.previousElementSibling) {
+            prev = prev.previousElementSibling;
+            prev.classList.add('hover');
+        }
+    });
+
+    star.addEventListener('mouseleave', () => {
+        stars.forEach(s => s.classList.remove('hover'));
+    });
+});
